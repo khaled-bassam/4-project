0ccd4f189366f87cc022adab7671d50e0909f68b
khaled-bassam <khaledabusitta2262003@gmail.com>
2026-06-06 19:23:46 +0300

    Add user routes with UserController actions

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
-<?php
+use App\Http\Controllers\UserController;
 
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('users')->name('users.')->group(function () {
+    Route::get('/', [UserController::class, 'index'])->name('index');
+    Route::get('/create', [UserController::class, 'create'])->name('create');
+    Route::post('/', [UserController::class, 'store'])->name('store');
+    Route::get('/{id}/edit', [UserController::class, 'edit'])->name('edit');
+    Route::put('/{id}', [UserController::class, 'update'])->name('update');
+    Route::delete('/{id}', [UserController::class, 'destroy'])->name('destroy');
 });
